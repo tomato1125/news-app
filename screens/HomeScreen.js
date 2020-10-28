6037fa092f7727b5fb68221a7b34d56f4c1aa6f3
tomato1125 <naokiyoshida1125@icoud.com>
2020-10-28 12:22:48 +0900

    fix

---
+++ screens/HomeScreen.js
@@ -0,0 +1,69 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import ListItem from '../components/ListItem';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: "row"
+  },
+  leftContainer:{
+    // backgroundColor: "red",
+    width: 100
+  },
+  rightContainer:{
+    padding: 10,
+    flex: 1,
+    justifyContent: "space-between"
+  },
+  text: {
+    fontSize: 16
+  },
+  subText: {
+    fontSize: 12,
+    color: "gray"
+  }
+});
+
+export default HomeScreen = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    alert(Constants.manifest.extra.newsApiKey);
+      fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+}
